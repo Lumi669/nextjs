a6e3b6279bca421122cc1efc10daf5d7b5f6df2a
cmjiwa <wangjinghuan@yahoo.com>
2022-10-10 19:25:09 +0300

    Add /, /events, events/<some-id>

---
+++ file-based-routing/pages/events/[someid].js
@@ -0,0 +1,9 @@
+function EventDetail() {
+  return (
+    <div>
+      <h1>Here is event detail of id 2</h1>
+    </div>
+  );
+}
+
+export default EventDetail;
---
+++ file-based-routing/pages/events/index.js
@@ -0,0 +1,9 @@
+const Events = () => {
+  return (
+    <div>
+      <h1>All the events</h1>
+    </div>
+  );
+};
+
+export default Events;
