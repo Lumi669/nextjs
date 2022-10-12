a73e5ffc9d11a8f0c5dc3e3335356b4e26c07477
cmjiwa <wangjinghuan@yahoo.com>
2022-10-12 14:13:33 +0300

    Add all events page

--- file-based-routing/pages/events/index.js
+++ file-based-routing/pages/events/index.js
@@ -1,9 +1,14 @@
-const Events = () => {
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
+const AllEventsPage = () => {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>All the events</h1>
+      <EventList items={events} />
     </div>
   );
 };
 
-export default Events;
+export default AllEventsPage;
